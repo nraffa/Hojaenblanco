f2ff358d76487483111313d090682d02458220ef
nraffa <nraffa@itba.edu.ar>
2019-06-07 18:20:07 -0300

    Aprendiendo base de datos con formato .CSV

---
+++ CSV.py
@@ -0,0 +1,30 @@
+#PROVINCIA,TRANSITORIAS,TEMPORARIAS,PERMANENTES,TOTAL
+import pandas as pd
+import numpy as np
+
+lectura = pd.read_csv('residencias-otorgadas---1-trimestre---ano-2019.csv', delimiter = ',', names = ['PROVINCIA','ABREVIACION','TRANSITORIAS','TEMPORARIAS','PERMANENTES','TOTAL'])
+#.head indica cuantas filas se imprimen (arrancando en 0 por supuesto)
+lectura1 = lectura.head(3)
+
+# Get the series
+#print lectura['PROVINCIA']
+prov = lectura['ABREVIACION']
+tran = lectura['TRANSITORIAS']
+print prov[2] #Cordoba
+#Filtering
+# print lectura[lectura.source == 'SAN JUAN'] no sale y no se porque
+
+
+import matplotlib.pyplot as plt
+
+#provin = [prov]
+#trans = [tran]
+x = prov
+y = tran
+plt.bar(x, y, label = 'Transitorias')
+
+plt.xlabel ('Provincias')
+plt.ylabel('Residencias Transitorias')
+plt.title('Residencias')
+plt.legend()
+plt.show()
